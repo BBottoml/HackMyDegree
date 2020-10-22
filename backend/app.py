14e255efde3e43a54b04d3620750ff779ee8f814
Bradley Bottomlee <bradley.bttm@gmail.com>
2020-10-22 18:15:18 -0400

    Create registration endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import json
 import os 
-from flask import Flask
+from flask import Flask, request
 from flask_mysqldb import MySQL
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -17,11 +17,33 @@ app.config['MYSQL_DB'] = os.environ.get("DB_NAME")
 
 mysql = MySQL(app)
 
-@app.route("/api/courses")
+@app.route("/api/courses", methods=["GET"])
 def get_courses():
     cur = mysql.connection.cursor()
     cur.execute('''SELECT course_title, course_name FROM Course''')
     return transform_to_json(cur)
+
+@app.route("/api/register", methods=["POST"])
+def sign_up():
+    data = request.get_json()
+    email = data["email"]
+    pswd = data["pswd"]
+
+    # determine if user already exists
+    cur = mysql.connection.cursor()
+    query = "SELECT user_id FROM User WHERE email = %s"
+    cur.execute(query, (email,))
+    result = cur.fetchall()
+    cur.close()
+    if result != ():
+        return json.dumps({'status': 'invalid'})
+    
+    # insert into user table
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO User(email, password) VALUES (%s, PASSWORD(%s))", (email, pswd))
+    mysql.connection.commit()
+    cur.close()
+    return json.dumps({'status': 'valid'})
 
 '''
 This function transforms SQL result to JSON for response
@@ -33,6 +55,7 @@ def transform_to_json(cur):
     json_data=[]
     for result in result:
         json_data.append(dict(zip(row_headers,result)))
+    cur.close()
     return json.dumps(json_data)
 
 
